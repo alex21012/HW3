945370b9ebc5707bdef128414045340720f3044f
alex21012 <66181239+alex21012@users.noreply.github.com>
2023-10-13 03:42:44 +0000

    commit

--- src/strPtr.c
+++ src/strPtr.c
@@ -1,22 +1,58 @@
 #include "global.h"
 
-int strgLen(char *s){
-    // TO BE IMPLEMENTED
-    abort();
+int strgLen(char *s) {
+    char *start = s;
+    while (*s) {
+        s++;
+    }
+    return s - start;
 }
-void strgCopy(char *s,char *d ){
-    // TO BE IMPLEMENTED
-    abort();
+
+void strgCopy(char *s, char *d) {
+    while (*s) {
+        *d = *s;
+        d++;
+        s++;
+    }
+    *d = '\0'; 
 }
-void strgChangeCase(char *s){
-    // TO BE IMPLEMENTED
-    abort();
+
+void strgChangeCase(char *s) {
+    char *current = s;
+    while (*current) {
+        if (isalpha(*current)) {
+            *current = isupper(*current) ? tolower(*current) : toupper(*current);
+        }
+        current++;
+    }
 }
-int strgDiff(char *s1,char *s2 ){
-    // TO BE IMPLEMENTED
-    abort();
+
+int strgDiff(char *s1, char *s2) {
+    int idx = 0;
+    while (*s1 && *s2) {
+        if (*s1 != *s2) {
+            return idx;
+        }
+        idx++;
+        s1++;
+        s2++;
+    }
+    if (*s1 != *s2) {
+        return idx;
+    }
+    return -1; // Strings are identical
 }
-void strgInterleave(char *s1, char *s2, char *d ){
-    // TO BE IMPLEMENTED
-    abort();
+
+void strgInterleave(char *s1, char *s2, char *d) {
+    while (*s1 && *s2) {
+        *d++ = *s1++;
+        *d++ = *s2++;
+    }
+    while (*s1) {
+        *d++ = *s1++;
+    }
+    while (*s2) {
+        *d++ = *s2++;
+    }
+    *d = '\0';  // Ensure the destination string is null-terminated
 }
