c65a572037f84dac3b0639c7b3c711b81f7ba926
alex21012 <66181239+alex21012@users.noreply.github.com>
2023-10-13 18:46:15 +0000

    Commit

--- src/ceasar.c
+++ src/ceasar.c
@@ -3,71 +3,13 @@
 /**
  *  Feel free to use the functions that you made in strPtr.c
  */ 
-int encrypt(const char *plaintext, char *ciphertext, int key) {
-    if (plaintext == NULL || ciphertext == NULL) {
-        return -2;
-    }
 
-    int count = 0;
-
-    while (*plaintext != '\0') {
-        if ('A' <= *plaintext && *plaintext <= 'Z') {
-            *ciphertext = ((*plaintext - 'A' + key) % 26) + 'A';
-        } else if ('a' <= *plaintext && *plaintext <= 'z') {
-            *ciphertext = ((*plaintext - 'a' + key) % 26) + 'a';
-        } else if ('0' <= *plaintext && *plaintext <= '9') {
-            *ciphertext = ((*plaintext - '0' + key) % 10) + '0';
-        } else {
-            *ciphertext = *plaintext;
-        }
-
-        plaintext++;
-        ciphertext++;
-        count++;
-    }
-
-    if (strgLen(ciphertext) + 7 > strgLen(ciphertext)) {
-        return -1;
-    }
-
-    strgCopy("__EOM__", ciphertext);
-
-    return count;
+int encrypt(const char *plaintext, char *ciphertext, int key){
+    // TO BE IMPLEMENTED
+    abort();
 }
 
-
-int decrypt(const char *ciphertext, char *plaintext, int key) {
-
-    if (plaintext == NULL || ciphertext == NULL) {
-        return -2;
-    }
-    if (!strstr(ciphertext, "__EOM__")) { 
-        return -1;
-    }
-
-    int count = 0;
-
-    while (*ciphertext != '\0' && count < strgLen(plaintext)) {
-        if (strncmp(ciphertext, "__EOM__", 7) == 0) {
-            break;
-        }
-
-        if ('A' <= *ciphertext && *ciphertext <= 'Z') {
-            *plaintext = ((*ciphertext - 'A' - key + 26) % 26) + 'A';
-        } else if ('a' <= *ciphertext && *ciphertext <= 'z') {
-            *plaintext = ((*ciphertext - 'a' - key + 26) % 26) + 'a';
-        } else if ('0' <= *ciphertext && *ciphertext <= '9') {
-            *plaintext = ((*ciphertext - '0' - key + 10) % 10) + '0';
-        } else {
-            *plaintext = *ciphertext;
-        }
-
-        ciphertext++;
-        plaintext++;
-        count++;
-    }
-
-    *plaintext = '\0';  
-
-    return count;
+int decrypt(const char *ciphertext, char *plaintext, int key){
+    // TO BE IMPLEMENTED
+    abort();
 }
